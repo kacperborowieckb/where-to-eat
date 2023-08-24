cd7535d617702904c892688ce525b6ad1281f8ff
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-08-24 18:41:40 +0200

    create RestaurantCard component

---
+++ src/components/restaurant-card/RestaurantCard.tsx
@@ -0,0 +1,77 @@
+import { RestaurantsDataType } from '../../features/api/apiSlice';
+import defaultImg from '../../assets/default.png';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Rating,
+  Typography,
+} from '@mui/material';
+
+const RestaurantCard = ({ restaurant }: { restaurant: RestaurantsDataType }) => {
+  return (
+    <Card sx={{ p: 1 }} variant="outlined">
+      <CardMedia
+        sx={{
+          width: '100%',
+        }}
+        component={'img'}
+        height={'120px'}
+        image={restaurant.photo ? restaurant.photo : defaultImg}
+        alt={`${restaurant.name} image`}
+      />
+      <CardContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          p: 1,
+          pb: '4px !important',
+        }}
+      >
+        <Typography component={'h4'} noWrap>
+          {restaurant.name}
+        </Typography>
+        <Typography component={'p'} variant="caption">
+          {restaurant.ranking}
+        </Typography>
+        {restaurant.num_reviews && (
+          <Typography component={'p'} variant="caption">
+            {restaurant.num_reviews + ' reviews'}
+          </Typography>
+        )}
+        <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+          <Typography component={'p'} variant="caption">
+            {restaurant.address}
+          </Typography>
+          <Typography component={'p'} variant="caption">
+            {restaurant.phone}
+          </Typography>
+        </Box>
+        {restaurant.is_closed && <Typography>{restaurant.is_closed}</Typography>}
+        <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+          <Typography>{restaurant.price_level}</Typography>
+          <Rating value={Number(restaurant.rating)} size="small" readOnly />
+        </Box>
+      </CardContent>
+      {restaurant.web_url && (
+        <CardActions>
+          <Button
+            href={restaurant.web_url}
+            sx={{ margin: '0 auto' }}
+            variant="outlined"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Visit site
+          </Button>
+        </CardActions>
+      )}
+    </Card>
+  );
+};
+
+export default RestaurantCard;
--- src/components/restaurants/Restaurants.tsx
+++ src/components/restaurants/Restaurants.tsx
@@ -1,17 +1,6 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid,
-  Rating,
-  Stack,
-  Typography,
-} from '@mui/material';
-import defaultImg from '../../assets/default.png';
+import { Grid, Stack } from '@mui/material';
 import { RestaurantsDataType } from '../../features/api/apiSlice';
+import RestaurantCard from '../restaurant-card/RestaurantCard';
 
 type RestaurantsProps = {
   restaurants: RestaurantsDataType[];
@@ -22,64 +11,7 @@ const Restaurants = ({ restaurants = [] }: RestaurantsProps) => {
     <Grid item xs={4} sx={{ overflowY: 'scroll', maxHeight: '100%', my: 2, pr: 0 }}>
       <Stack spacing={2} px={1} pb={3}>
         {restaurants.map((restaurant) => (
-          <Card sx={{ p: 1 }} variant="outlined" key={restaurant.location_id}>
-            <CardMedia
-              sx={{
-                width: '100%',
-              }}
-              component={'img'}
-              height={'120px'}
-              image={restaurant.photo ? restaurant.photo : defaultImg}
-              alt={`${restaurant.name} image`}
-            />
-            <CardContent
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1,
-                p: 1,
-                pb: '4px !important',
-              }}
-            >
-              <Typography component={'h4'} noWrap>
-                {restaurant.name}
-              </Typography>
-              <Typography component={'p'} variant="caption">
-                {restaurant.ranking}
-              </Typography>
-              {restaurant.num_reviews && (
-                <Typography component={'p'} variant="caption">
-                  {restaurant.num_reviews + ' reviews'}
-                </Typography>
-              )}
-              <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <Typography component={'p'} variant="caption">
-                  {restaurant.address}
-                </Typography>
-                <Typography component={'p'} variant="caption">
-                  {restaurant.phone}
-                </Typography>
-              </Box>
-              {restaurant.is_closed && <Typography>{restaurant.is_closed}</Typography>}
-              <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                <Typography>{restaurant.price_level}</Typography>
-                <Rating value={Number(restaurant.rating)} size="small" readOnly />
-              </Box>
-            </CardContent>
-            {restaurant.web_url && (
-              <CardActions>
-                <Button
-                  href={restaurant.web_url}
-                  sx={{ margin: '0 auto' }}
-                  variant="outlined"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Visit site
-                </Button>
-              </CardActions>
-            )}
-          </Card>
+          <RestaurantCard restaurant={restaurant} key={restaurant.location_id} />
         ))}
       </Stack>
     </Grid>
